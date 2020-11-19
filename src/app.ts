58a6c33e4bf5522cb01f0accd72bb341b440bd17
ohmink <kkop5056@naver.com>
2020-11-19 18:47:23 +0900

    feat: passport 및 router 추가 passport를 이용한 Oauth 인증을 구현하기 위한 코드 추가 authRouter를 사용하는 코드 추가 기타 설정 추가

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
 import * as express from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as passport from 'passport';
+
+import initPassport from './passport';
+
+import authRouter from './routes/auth/Login';
 
 const app: express.Application = express();
+
+initPassport();
+app.use('/', authRouter);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(passport.initialize());
 
 app.get(
   '/',
