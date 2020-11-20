58928dfc753748283428266a053233ed2ebb667b
ohmink <kkop5056@naver.com>
2020-11-20 11:25:19 +0900

    refactor: 간격 수정, 라우터 변경 authRouter -> routerBundle 기타 줄 간격 수정

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,18 @@
 import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
 import * as passport from 'passport';
-
 import initPassport from './passport';
-
-import authRouter from './routes/auth/Login';
+import routerBundle from './routes/index';
 
 const app: express.Application = express();
 
 initPassport();
-app.use('/', authRouter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(passport.initialize());
 
-app.get(
-  '/',
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.send('hello typescript express!');
-  }
-);
+app.use('/', routerBundle);
 
 export default app;
