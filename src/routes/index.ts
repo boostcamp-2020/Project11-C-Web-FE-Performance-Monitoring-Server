9b04ae756ef880ebce3731b8145bf0b80e5e52d6
JeesooHaa <haajeesoo@gmail.com>
2020-11-30 19:23:32 +0900

    feat: User 조회 로직 추가
     User 정보를 가져오는데 사용할 API를 추가했습니다.
     User 정보는 passport 미들웨어에서 넘겨준 정보를 반환합니다.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,26 @@
 import * as express from 'express';
+import * as passport from 'passport';
 import loginRouter from './auth/Login';
 import logRouter from './log/Log';
+<<<<<<< HEAD
 import projectRouter from './ProjectRoute';
+=======
+import userRouter from './UserRoute';
+>>>>>>> feat: User 조회 로직 추가
 
 const router: express.Router = express();
 
 router.use('/oauth', loginRouter);
 router.use('/log', logRouter);
+<<<<<<< HEAD
 router.use('/project', projectRouter);
+=======
+router.use(
+  '/user',
+  passport.authenticate('jwt', { session: false }),
+  userRouter
+);
+>>>>>>> feat: User 조회 로직 추가
 
 router.get('/', (req: express.Request, res: express.Response) => {
   res.send('hello typescript express!');
