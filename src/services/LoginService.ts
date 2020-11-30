ec28819080525234539028b72e9acc07a543f376
JeesooHaa <haajeesoo@gmail.com>
2020-11-30 19:15:25 +0900

    fix: JWT 발급 과정 수정
     JWT에 user의 id가 저장되도록 수정했습니다.
     redirect시 JWT를 함께 전달하기 위해서 query에 JWT를 추가했습니다.
     실제 사용할 google과 github oAuth에만 변경사항을 적용했습니다.

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -1,25 +1,25 @@
 import User from '../models/User';
 
-const loginService = {
-  saveData: async (user: any, domain: string) => {
-    const { id, displayName, username, emails, photos } = user;
-    const [email] = emails || [{ value: null }];
-    const [photo] = photos;
-    const oauthId = `${id}_${domain}`;
+const saveData = async (user: any, domain: string) => {
+  const { id, displayName, username, emails, photos } = user;
+  const [email] = emails || [{ value: null }];
+  const [photo] = photos;
+  const oauthId = `${id}_${domain}`;
 
-    const conditions = { oauthId };
-    const docs = Object({
-      name: displayName || username,
-      email: email.value,
-      imageURL: photo.value,
-      oauthId,
-      status: true,
-    });
+  const conditions = { oauthId };
+  const docs = Object({
+    name: displayName || username,
+    email: email.value,
+    imageURL: photo.value,
+    oauthId,
+    status: true,
+  });
 
-    const one = await User.findOne(conditions);
-    const userData = one || (await User.create(docs));
-    return userData;
-  },
+  const one = await User.findOne(conditions);
+  const temp = one || (await User.create(docs));
+  const userId = temp._id;
+  const userStatus = temp.status;
+  return [userId, userStatus];
 };
 
-export default loginService;
+export default { saveData };
