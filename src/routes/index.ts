3483cd01834c997fd44260457247538c43e354ef
Dongmin Shin <nsdm821@gmail.com>
2020-12-01 23:02:48 +0900

    feat: errorevent, issue router , test url 추가 /errorevent : error event 관련 /issue : issue 관련 /test :인위적으로 error 발생시켜 테스트

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,22 @@
 import * as express from 'express';
 import loginRouter from './auth/Login';
 import logRouter from './log/Log';
+import errorEventRouter from './Error';
+import issueRouter from './Issue';
 
 const router: express.Router = express();
-
 router.use('/oauth', loginRouter);
 router.use('/log', logRouter);
+router.use('/errorevent', errorEventRouter);
+router.use('/issue', issueRouter);
 
 router.get('/', (req: express.Request, res: express.Response) => {
   res.send('hello typescript express!');
 });
-
+router.get(
+  '/test',
+  (req: any, res: express.Response, next: express.NextFunction) => {
+    throw new Error('testets');
+  }
+);
 export default router;
