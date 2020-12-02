477d753c85944501b37956fc4aff500910b88d91
Dongmin Shin <nsdm821@gmail.com>
2020-12-02 21:18:25 +0900

    refactor: service class 를 function으로 바꿈 다른 service 와의 코딩 스타일 일치를 위해 변경

--- src/services/ErrorEventService.ts
+++ src/services/ErrorEventService.ts
@@ -1,30 +1,25 @@
 import * as crpyto from 'crypto';
 import ErrorEvent, { ErrorEventDocument } from '../models/ErrorEvent';
 
-class ErrorEventService {
-  saveErrorEvent = async (LogData: {}) => {
-    const eventId: string = crpyto
-      .createHash('md5')
-      // eslint-disable-next-line dot-notation
-      .update(LogData['content'])
-      .digest('hex');
-    const newErrorEvent = new ErrorEvent({ ...LogData, eventId });
-    await newErrorEvent.save();
-    return newErrorEvent;
-  };
+export const saveErrorEvent = async (LogData: {}) => {
+  const eventId: string = crpyto
+    .createHash('md5')
+    // eslint-disable-next-line dot-notation
+    .update(LogData['content'])
+    .digest('hex');
+  const newErrorEvent = new ErrorEvent({ ...LogData, eventId });
+  await newErrorEvent.save();
+  return newErrorEvent;
+};
 
-  getErrorEvent = async (eventId: String) => {
-    const res: ErrorEventDocument = await ErrorEvent.findOne({
-      eventId,
-    }).exec();
-    return res;
-  };
+export const getErrorEvent = async (eventId: String) => {
+  const res: ErrorEventDocument = await ErrorEvent.findOne({
+    eventId,
+  }).exec();
+  return res;
+};
 
-  getAllErrorEvent = async () => {
-    const res: ErrorEventDocument[] = await ErrorEvent.find().exec();
-    return res;
-  };
-}
-
-const errorEventService = new ErrorEventService();
-export default errorEventService;
+export const getAllErrorEvent = async () => {
+  const res: ErrorEventDocument[] = await ErrorEvent.find().exec();
+  return res;
+};
--- src/services/IssueService.ts
+++ src/services/IssueService.ts
@@ -1,55 +1,52 @@
 import Issue, { IssueDocument } from '../models/Issue';
 import { ErrorEventDocument } from '../models/ErrorEvent';
 
-class IssueService {
-  // 기존 issue가 없을 경우 새로 만들어낸다
-  createIssue = async (errorEvent: ErrorEventDocument) => {
-    const title = errorEvent.content.split('\n')[0];
-    const msg = errorEvent.content.split('\n')[1];
+// 기존 issue가 없을 경우 새로 만들어낸다
+export const createIssue = async (errorEvent: ErrorEventDocument) => {
+  const title = errorEvent.content.split('\n')[0];
+  const msg = errorEvent.content.split('\n')[1];
 
-    const newIssue = new Issue({
-      eventId: errorEvent.eventId,
-      title,
-      description: msg,
-      comments: [],
-      errorEvents: [errorEvent],
-    });
+  const newIssue = new Issue({
+    eventId: errorEvent.eventId,
+    title,
+    description: msg,
+    comments: [],
+    errorEvents: [errorEvent],
+  });
 
-    const result = await newIssue.save();
-    return result;
-  };
+  const result = await newIssue.save();
+  return result;
+};
 
-  addErrorEventToISsue = async (errorEvent: ErrorEventDocument) => {
-    if (!(await this.hasIssue(errorEvent.eventId))) {
-      return this.createIssue(errorEvent);
-    }
-    return this.appendErrorEventToIssue(errorEvent);
-  };
+export const appendErrorEventToIssue = async (
+  errorEvent: ErrorEventDocument
+) => {
+  const res = await Issue.findOneAndUpdate(
+    { eventId: errorEvent.eventId },
+    { $push: { errorEvents: errorEvent } }
+  );
+  return res;
+};
 
-  appendErrorEventToIssue = async (errorEvent: ErrorEventDocument) => {
-    const res = await Issue.findOneAndUpdate(
-      { eventId: errorEvent.eventId },
-      { $push: { errorEvents: errorEvent } }
-    );
-    return res;
-  };
+export const getIssue = async (eventId: String) => {
+  const res: IssueDocument = await Issue.findOne({ eventId }).exec();
+  return res;
+};
 
-  getIssue = async (eventId: String) => {
-    const res: IssueDocument = await Issue.findOne({ eventId }).exec();
-    return res;
-  };
+export const getAllIssue = async () => {
+  const res: IssueDocument[] = await Issue.find({}).exec();
+  return res;
+};
 
-  getAllIssue = async () => {
-    const res: IssueDocument[] = await Issue.find({}).exec();
-    return res;
-  };
+export const hasIssue = async (eventId: String) => {
+  const res: IssueDocument = await Issue.findOne({ eventId }).exec();
+  console.log('issue', !!res);
+  return !!res;
+};
 
-  hasIssue = async (eventId: String) => {
-    const res: IssueDocument = await Issue.findOne({ eventId }).exec();
-    console.log('issue', !!res);
-    return !!res;
-  };
-}
-
-const issueService = new IssueService();
-export default issueService;
+export const addErrorEventToISsue = async (errorEvent: ErrorEventDocument) => {
+  if (!(await hasIssue(errorEvent.eventId))) {
+    return createIssue(errorEvent);
+  }
+  return appendErrorEventToIssue(errorEvent);
+};
