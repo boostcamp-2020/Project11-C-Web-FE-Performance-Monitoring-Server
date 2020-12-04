723a663c5c0d80af73d012a4813b6c6082a4a62a
Dongmin Shin <nsdm821@gmail.com>
2020-12-04 10:50:34 +0900

    feat: cors 미들웨어 및 설정 추가

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
 import * as passport from 'passport';
+import * as cors from 'cors';
+// import * as acent from '@acent/node';
 import initPassport from './passport';
 import routerBundle from './routes/index';
 import getConnection from './mongo';
@@ -8,12 +10,18 @@ import getConnection from './mongo';
 const app: express.Application = express();
 getConnection();
 initPassport();
-
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+// acent.init({ dsn: 'http://localhost:3000/errorevent' });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(passport.initialize());
 
 app.use('/', routerBundle);
-
+// app.use(acent.Handlers.errorHandler);
 export default app;
