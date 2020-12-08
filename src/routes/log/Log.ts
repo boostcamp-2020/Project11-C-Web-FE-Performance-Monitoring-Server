ef7ac49cd52b7f12968a8c2eff48c3b403ca5584
Dongmin Shin <nsdm821@gmail.com>
2020-12-08 22:00:38 +0900

    fix: log deprecated 더이상 사용하지 않는 log 관련 코드 삭제

--- src/routes/log/Log.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express';
-import LogController from '../../controllers/LogControllers';
-
-const router: express.Router = express();
-
-router.post('/', LogController.collectLog);
-router.get('/', LogController.getLogs);
-export default router;
